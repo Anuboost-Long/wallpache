9bd6a5a325eb6227f92c97bd3b2dc10130616e3b
Ly kimlong <104675801+Anuboost-Long@users.noreply.github.com>
2026-07-28 23:53:48 +0700

    Initial Commit

---
+++ Wallpache/WallpacheApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallpacheApp.swift
+//  Wallpache
+//
+//  Created by Infinity General Insurance on 28/7/26.
+//
+
+import SwiftUI
+
+@main
+struct WallpacheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
